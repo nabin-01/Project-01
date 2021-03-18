83c79cd2648899ce6fa3cdcbf6b03978d3d0375b
nabin-01 <nabin1001@gmail.com>
2021-03-18 12:27:41 +0545

    first commit

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from . import views
+from .views import *
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', home, name='home'),
+    path('about/', about, name='about'),
 ]
