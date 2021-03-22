383ea40cae2585edeaa7a9b1b955677ba96579fa
nabin-01 <nabinghimire1001@gmail.com>
2021-03-22 12:10:36 +0545

    committed

--- home/models.py
+++ home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
+    objects = None
     name = models.CharField(max_length=300)
     email = models.CharField(max_length=300)
     subject = models.TextField()
--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import *
-
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -23,19 +23,25 @@ def blog_single(request):
     return render(request, 'blog-single.html')
 
 
-def Contact(request):
+def contact(request):
+    # 'POST' means form is being submitted
     if request.method == 'POST':
         name = request.POST['name']
         email = request.POST['email']
         sub = request.POST['subject']
         msg = request.POST['message']
+
         data = Contact.objects.create(
             name=name,
             email=email,
             subject=sub,
             message=msg
         )
+        # check entered user data is correct or not by python validators
+        # saves form data to DB
         data.save()
+        messages.add_message(request, messages.SUCCESS, '✔️ SUCCESSFUL SUBSCRIPTION')
+    # if response it render it looks for a HTML
     return render(request, 'contact.html')
 
 
